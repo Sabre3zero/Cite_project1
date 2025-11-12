953bde084777271e9865ca1959cf3921a1d1432c
Boris <108396167+Yashertasher@users.noreply.github.com>
2025-11-12 16:50:33 +0300

    db link test

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+
 function change(element){
     const tmp = element.textContent;    
 
@@ -19,11 +23,29 @@ function change(element){
     }
 }
 
+function testDB(){
+    const db = open({
+        filename: 'DB\TeaPopic.db',
+        driver: sqlite3.Database
+    });
+
+    alert(db.exec('SELECT * FROM Price_list'))
+}
+
 
 const editableElements = document.querySelectorAll('.changable')
 
 editableElements.forEach(function(element){
     element.addEventListener('click', function(event){   
         change(element);
+    });
+});
+
+
+const testButton = document.querySelectorAll('.test_button')
+
+testButton.forEach(function(element){
+    element.addEventListener('click', function(event){
+        testDB();
     });
 });
